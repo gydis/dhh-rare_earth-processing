39cc02da098671073675c54ba9e260c98f2820c0
gydis <egor.eremin@aalto.fi>
2025-05-18 23:40:48 +0300

    Refactor language normzalization functions

--- lang_funcs.py
+++ lang_funcs.py
@@ -1,36 +1,59 @@
+def lang_to_func(lang):
+    match lang:
+        case "rus_Cyrl":
+            return rus_normalize
+        case "eng_Latn":
+            raise ValueError("English normalization is not implemented yet.")
+        case "fin_Latn":
+            return ValueError("Finnish normalization is not implemented yet.")
+        case "zho_Hans":
+            return normalize_chinese
+        case "deu_Latn":
+            return normalize_german
+        case "ben_Beng":
+            return normalize_bengali
+        case "hin_Deva":
+            return ValueError("Hindi normalization is not implemented yet.")
+        case _:
+            raise ValueError(f"Language {lang} is not supported.")
+
 import spacy
-
+import string
 nlp_ru = spacy.load("ru_core_news_sm", disable=["parser", "ner"])
-
 def rus_normalize(text):
     text = text.lower()
-    # get rid of punctuation
     text.replace('\n', ' ')
-    print(text)
-    text = ''.join(list(filter(lambda x: x.isalpha() or x == ' ' or x == '-', text)))
-    doc = nlp_ru(text)
-    normed_text = ''.join([token.lemma_ for token in doc])
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    doc = nlp_ru(text.lower())
+    normed_text = ' '.join([token.lemma_ for token in doc])
     return normed_text
 
 #pip install pandas
 #pip install spacy
 #python -m spacy download de_core_news_sm
 
-import pandas as pd
-import spacy
-nlp = spacy.load('de_core_news_sm')
+# nlp_de = spacy.load('de_core_news_sm', disable=["parser", "ner"])
 
 def normalize_german(text):
     text.replace('\n', ' ')
-    doc = nlp(text.lower())
+    doc = nlp_de(text.lower())
     lemmas = [token.lemma_ for token in doc]
     return " ".join(lemmas)
 
+import pkuseg
 
-def process_german_keywords(csv_file: str) -> list:
-    df = pd.read_csv(csv_file)
-    normalized_keywords = []
-    for keyword in df['German']:
-        normalized = normalize_german(keyword)
-        normalized_keywords.append(normalized)
-    return normalized_keywords
+def normalize_chinese(text:str):
+    seg = pkuseg.pkuseg()         
+    stems = seg.cut(text)  
+    return " ".join(stems)
+
+
+# from bangla_stemmer.stemmer import stemmer
+# stmr = stemmer.BanglaStemmer()
+def normalize_bengali(text):
+    words = text.split()
+    stems = [ ]
+    for word in words:
+        stm = stmr.stem(word)
+        stems.append(stm)
+    return " ".join(stems)
